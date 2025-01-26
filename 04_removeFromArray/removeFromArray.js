afc467ba776726bae7a46fc1cda6335f5fe511ed
Danijel Gomola <195056091+dg0mke@users.noreply.github.com>
2025-01-26 19:18:59 +0100

    Remove elements from array
    removeFromArray() function takes as first argument
    as an array and other argument(s) that represents
    the element(s) that should be removed from the
    array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const removedElements = [...args];
+    const newArray = array.filter(element => !removedElements.includes(element));
+
+    return newArray;
 };
 
 // Do not edit below this line
