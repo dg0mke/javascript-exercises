f6f90958da26a9e851e5d67081c9c7c7acd8ae9b
Danijel Gomola <195056091+dg0mke@users.noreply.github.com>
2025-01-26 19:44:44 +0100

    Sum integers
    sumAll() function takes two arguments as integers
    and sum all integers between them (including them).

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
 
+    if (startNum < 0 || endNum < 0 || Number.isInteger(startNum) === false || Number.isInteger(endNum) === false || Number.isNaN(startNum) || Number.isNaN(endNum)) {
+        return "ERROR";
+    } else {
+        if (startNum < endNum) {
+            for (let i = startNum; i <= endNum; i++) {
+                sum += i;
+            }
+        } else if (startNum > endNum) {
+            for (let i = startNum; i >= endNum; i--) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
 };
 
 // Do not edit below this line
