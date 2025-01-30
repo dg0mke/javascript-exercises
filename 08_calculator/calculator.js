c0abe482c016fa1ae62786ab0a471ff29aad1d77
Danijel Gomola <195056091+dg0mke@users.noreply.github.com>
2025-01-30 21:15:38 +0100

    Create simple calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
 const add = function() {
-	
+	return arguments[0] + arguments[1];
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1];
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total * current, 1);
 };
 
 const power = function() {
-	
+	return arguments[0] ** arguments[1];
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+	if (fact === 0) {
+    return 1;
+  } else {
+    let i = 1;
+    let product = 1;
+
+    while (i <= fact) {
+      product *= i;
+      i++;
+    }
+
+    return product;
+  }
 };
 
 // Do not edit below this line
