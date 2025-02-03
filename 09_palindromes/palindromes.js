7f0282e7edc7c3997726080d5df55d69a9e357b6
Danijel Gomola <195056091+dg0mke@users.noreply.github.com>
2025-02-03 20:55:17 +0100

    Determines whether given string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanum = "123567890qwertyuiopasdfghjklzxcvbnm";
 
+    const newString = string
+        .toLowerCase()
+        .split("")
+        .filter(char => alphanum.includes(char))
+        .join("");
+
+    const reversedString = newString
+        .split("")
+        .reverse()
+        .join("");
+
+    return newString === reversedString;
 };
 
 // Do not edit below this line
