047a43a07b9e7f7f00499658b867c8df7d718d78
Danijel Gomola <195056091+dg0mke@users.noreply.github.com>
2025-02-03 21:18:26 +0100

    Return specific number of Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS";
+    } else {
+        const fib = [0, 1];
 
+        for (let i = 2; i <= index; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        return fib[index];
+    }
 };
 
 // Do not edit below this line
